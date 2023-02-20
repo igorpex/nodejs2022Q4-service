96fc2f0683e4ea355f611ab4071d51f5929efff0
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-20 17:27:30 +0300

    feat: update tracks

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -5,14 +5,15 @@ import {
 } from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
-import { Track } from './entities/track.entity';
+// import { Track } from './entities/track.entity';
 import { v4 as uuid, validate, version } from 'uuid';
-import { db } from 'src/data/db';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TracksService {
+  constructor(private prisma: PrismaService) {}
   // private tracks: Array<Track> = [];
-  create(createTrackDto: CreateTrackDto) {
+  async create(createTrackDto: CreateTrackDto) {
     // return 'This action adds a new track';
     //check required fields and types
     const hasAllRequiredFields = createTrackDto.name && createTrackDto.duration;
@@ -27,43 +28,42 @@ export class TracksService {
     }
 
     // create a track with random id
-    const newTrack = {
+    const newTrackData = {
       id: uuid(),
       ...createTrackDto,
     };
-    db.tracks.push(newTrack);
+    // db.tracks.push(newTrack);
+    const newTrack = await this.prisma.tracks.create({
+      data: newTrackData,
+    });
     return newTrack;
   }
 
-  findAll() {
-    // return `This action returns all tracks`;
-    return db.tracks;
+  async findAll() {
+    const tracks = await this.prisma.tracks.findMany();
+    return tracks;
   }
 
-  findOne(id: string) {
-    // return `This action returns a #${id} track`;
+  async findOne(id: string) {
     if (!validate(id) || !(version(id) === 4)) {
       throw new BadRequestException(`trackId ${id} is invalid (not uuid)`);
     }
-
-    // Check track exists
-    const track: Track = db.tracks.find((track) => track['id'] === id);
+    const track = await this.prisma.tracks.findUnique({ where: { id } });
     if (!track) {
-      throw new NotFoundException('Track not found.');
+      throw new NotFoundException('Track not found.'); //404
     }
     return track;
   }
 
-  update(id: string, updateTrackDto: UpdateTrackDto) {
+  async update(id: string, updateTrackDto: UpdateTrackDto) {
     // return `This action updates a #${id} track`;
     // check uuid is valid or return error
     if (!validate(id) || !(version(id) === 4)) {
       throw new BadRequestException(`trackId ${id} is invalid (not uuid)`); //400
     }
     // Check track exists
-    const index: number = db.tracks.findIndex((track) => track['id'] === id);
-    // const track: Track = db.tracks.find((track) => track['id'] === id);
-    if (index === -1) {
+    const track = await this.prisma.tracks.findUnique({ where: { id } });
+    if (!track) {
       throw new NotFoundException('Track not found.'); //404
     }
 
@@ -80,39 +80,33 @@ export class TracksService {
     }
 
     //create updated track
-    const updatedTrack = {
+    const updatedTrackData = {
       id: id,
       ...updateTrackDto,
     };
-    db.tracks[index] = updatedTrack;
+    const updatedTrack = await this.prisma.tracks.update({
+      data: updatedTrackData,
+      where: { id },
+    });
     return updatedTrack;
   }
 
-  remove(id: string) {
+  async remove(id: string) {
     // return `This action removes a #${id} track`;
     // check uuid is valid or return error
     if (!validate(id) || !(version(id) === 4)) {
       throw new BadRequestException(`trackId ${id} is invalid (not uuid)`); //400
     }
-    // Check track exists
-    const track: Track = db.tracks.find((track) => track['id'] === id);
-    if (!track) {
+
+    try {
+      await this.prisma.tracks.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      // console.log('####tracks.service remove error:', error);
       throw new NotFoundException('Track not found.'); //404
     }
-
-    // Null links in related entities
-    db.artists = db.artists.map((artist) => {
-      if (artist.trackId === id) artist.trackId = null;
-      return artist;
-    });
-
-    // Remove from favorites
-    db.favorites.tracks = db.favorites.tracks.filter(
-      (trackId) => trackId !== id,
-    );
-
-    // Remove track itself
-    db.tracks = db.tracks.filter((track) => track['id'] !== id);
-    return;
   }
 }
