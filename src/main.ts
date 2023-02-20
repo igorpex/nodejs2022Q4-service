e4774ae5aa7b707eed4cbd9af9bdec32d6e17fb7
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-20 07:01:13 +0300

    fix: update ValidationPipe params due class-validator upgrade
    According to info here:
    https://github.com/nestjs/nest/issues/10683

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({}));
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   await app.listen(PORT);
 }
 bootstrap();
