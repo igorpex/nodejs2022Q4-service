7814298546e5d8bb9493ba92de17071567efd798
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-20 20:49:56 +0300

    fix: update BigInt Prisma issue
    Prisma cannot correctly transfer BigInt to Number, use workaround

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 const PORT = process.env.PORT || 4000;
 
+BigInt.prototype['toJSON'] = function () {
+  return Number(this.toString());
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
