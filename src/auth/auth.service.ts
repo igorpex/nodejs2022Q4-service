f285313f77717c58657e5d03876a9745d5b6f394
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-26 13:31:54 +0300

    feat: add refresh token service
    refresh token required in body

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,12 +5,12 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-// import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { LoginUserDto } from './dto/login-user.dto';
-import { JwtAuthGuard } from './guards/jwt-guard';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
@@ -18,18 +18,23 @@ export class AuthService {
     private prisma: PrismaService,
     private readonly usersService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
   async signup(createUserDto: CreateUserDto) {
-    // createUserDto.password = await this.usersService.hashPassword(
-    //   createUserDto.password,
-    // );
     return await this.usersService.create(createUserDto);
   }
 
+  createTokens(payload) {
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_SECRET_KEY'),
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_SECRET_REFRESH_KEY'),
+    });
+    return { accessToken, refreshToken };
+  }
+
   async login(loginUserDto: LoginUserDto) {
-    // createUserDto.password = await this.usersService.hashPassword(
-    //   createUserDto.password,
-    // );
     const user = await this.prisma.users.findFirst({
       where: {
         login: loginUserDto.login,
@@ -46,8 +51,36 @@ export class AuthService {
     if (!validatePassword) {
       throw new ForbiddenException('Not correct combination'); //403
     }
-    // console.log('user at login:', user);
     const payload = { userId: user.id, login: user.login };
-    return { accessToken: this.jwtService.sign(payload) };
+    const tokens = this.createTokens(payload);
+    return tokens;
+  }
+
+  private verifyRefreshToken(refreshToken: string) {
+    console.log('refreshToken', refreshToken);
+    try {
+      const verify = this.jwtService.verify(refreshToken, {
+        secret: this.configService.get('JWT_SECRET_REFRESH_KEY'),
+      });
+      return verify;
+    } catch (error) {}
+  }
+
+  async refresh(refreshTokenDto: RefreshTokenDto) {
+    const decryptedToken = this.verifyRefreshToken(
+      refreshTokenDto.refreshToken,
+    );
+    if (!decryptedToken) {
+      throw new ForbiddenException('Wrong token');
+    } //403
+
+    const userId = decryptedToken.userId;
+    const user = await this.usersService.findOne(userId);
+    if (!user) {
+      throw new ForbiddenException(`Wrong user`);
+    } //403
+    const payload = { userId: user.id, login: user.login };
+    const tokens = this.createTokens(payload);
+    return tokens;
   }
 }
