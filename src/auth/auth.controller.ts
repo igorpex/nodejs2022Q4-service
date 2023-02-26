3232104a322d045fc86d093a155365fdd988414e
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-26 13:29:16 +0300

    feat: set @Public() to open routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,23 +3,27 @@ import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @Public()
   @Post('signup')
   register(@Body() createUserDto: CreateUserDto) {
     return this.authService.signup(createUserDto);
   }
 
-  //   @Post('login')
-  //   login(@Request() req) {
-  //     return this.authService.login(req.user);
-  //   }
-//   @UseGuards(AuthGuard('local'))
+  @Public()
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Public()
+  @Post('refresh')
+  refresh(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refresh(refreshTokenDto);
   }
 }
