9051828cffb9996a7ba0df685d5d29236300846d
Igor Bogdanov <igor.bogdanov@gmail.com>
2023-02-27 21:46:49 +0300

    feat: listener and logging unhandledRejection and uncaughtException

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { MyloggerService } from './mylogger/mylogger.service';
+
 const PORT = process.env.PORT || 4000;
 
 BigInt.prototype['toJSON'] = function () {
@@ -14,7 +15,19 @@ async function bootstrap() {
     bufferLogs: true,
   });
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
-  app.useLogger(new MyloggerService());
+
+  const myloggerService = new MyloggerService();
+  app.useLogger(myloggerService);
+
+  process.on('unhandledRejection', async (err) => {
+    myloggerService.warn(err, 'Unhandled Rejection');
+  });
+
+  process.on('uncaughtException', async (err, origin) => {
+    myloggerService.error(`${err}. Origin: ${origin}.`, 'Uncaught exception');
+  });
+
   await app.listen(PORT);
 }
+
 bootstrap();
